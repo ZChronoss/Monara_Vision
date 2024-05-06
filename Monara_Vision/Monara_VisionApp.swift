ff8bcd992115fd9221a6cec9649eade285637bb3
Renaldi <renaldi.antonio19@gmail.com>
2024-05-06 21:02:08 +0700

    Initial Commit

---
+++ Monara_Vision/Monara_VisionApp.swift
@@ -0,0 +1,21 @@
+//
+//  Monara_VisionApp.swift
+//  Monara_Vision
+//
+//  Created by Renaldi Antonio on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Monara_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
